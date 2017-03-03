9dc3f7946a25b077f0712c629219941d5795aaf3
Israel Vidal <israelcvidal@gmail.com>
2017-03-03 14:24:21 -0300

    Adding comments, adding geolocation.csv

--- src/test/ConsumerTest.java
+++ src/test/ConsumerTest.java
@@ -2,11 +2,16 @@ package test;
 
 import kafka.consumer.ConsumerIterator;
 import kafka.consumer.KafkaStream;
- 
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+
 public class ConsumerTest implements Runnable {
     private KafkaStream m_stream;
     private int m_threadNumber;
- 
+    private HashMap<String, Integer> counter = new HashMap<>();
+    
     public ConsumerTest(KafkaStream a_stream, int a_threadNumber) {
         m_threadNumber = a_threadNumber;
         m_stream = a_stream;
@@ -14,8 +19,19 @@ public class ConsumerTest implements Runnable {
  
     public void run() {
         ConsumerIterator<byte[], byte[]> it = m_stream.iterator();
-        while (it.hasNext())
-            System.out.println("Thread " + m_threadNumber + ": " + new String(it.next().message()));
+        String message;
+        while (it.hasNext()){
+        	message = new String(it.next().message());
+//        	Contando o número de vezes que uma cidade aparece ao longo de toda a stream
+        	if(counter.containsKey(message)){
+        	     counter.put(message, counter.get(message)+1);
+        	}
+        	else{
+        	    counter.put(message, 1);
+        	}
+        }
+        
+        System.out.println(counter);
         System.out.println("Shutting down Thread: " + m_threadNumber);
     }
 }
--- src/test/TestProducer.java
+++ src/test/TestProducer.java
@@ -4,44 +4,16 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
+import java.util.StringTokenizer;
 
 import kafka.javaapi.producer.Producer;
 import kafka.producer.KeyedMessage;
 import kafka.producer.ProducerConfig;
  
 public class TestProducer {
-//    public static void main(String[] args) {
-//        long events = Long.parseLong(args[0]);
-//        Random rnd = new Random();
-//        String path = "/home/local/LSBD/israel.vidal/Área de Trabalho/test.txt";
-// 
-//        Properties props = new Properties();
-//        props.put("metadata.broker.list", "localhost:9092,localhost:9092 ");
-//        props.put("serializer.class", "kafka.serializer.StringEncoder");
-//        props.put("partitioner.class", "test.SimplePartitioner");
-//        props.put("request.required.acks", "1");
-// 
-//        ProducerConfig config = new ProducerConfig(props);
-// 
-//        Producer<String, String> producer = new Producer<String, String>(config);
-// 
-//        for (long nEvents = 0; nEvents < events; nEvents++) { 
-//               long runtime = new Date().getTime();  
-//               String ip = "192.168.2." + rnd.nextInt(255); 
-//               String msg = runtime + ",www.example.com," + ip; 
-//               KeyedMessage<String, String> data = new KeyedMessage<String, String>("test", ip, msg);
-//               producer.send(data);
-//               try {
-//				Thread.sleep(10000);
-//			} catch (InterruptedException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//        producer.close();
-//    }
 	
 	public static void main(String[] args) throws InterruptedException {
-        String filePath = "/home/local/LSBD/israel.vidal/Área de Trabalho/test.txt";
+        String filePath = "/home/local/LSBD/israel.vidal/workspace/Stream/geolocation.csv";
  
         Properties props = new Properties();
         props.put("metadata.broker.list", "localhost:9092,localhost:9092 ");
@@ -54,19 +26,21 @@ public class TestProducer {
         Producer<String, String> producer = new Producer<String, String>(config);
  
         try{
+//        	Lendo o arquivo para usar como simulação de streaming
         	BufferedReader reader = new BufferedReader(new FileReader(filePath));
         	List<String> records = new ArrayList<String>();	
         	String line;
+        	
         	while ((line = reader.readLine()) != null){
-        		records.add(line);
+        		String city = line.split(",")[3];
+        		records.add(city);
         	}
         	reader.close();
         	
+//        	Simulando produção de localização com os dados do arquivo
             for (String msg : records) {
-                		   
             	KeyedMessage<String, String> data = new KeyedMessage<String, String>("test", msg);
             	producer.send(data);
-//    			Thread.sleep(1000);
     		}
         	
         } catch (IOException e){
